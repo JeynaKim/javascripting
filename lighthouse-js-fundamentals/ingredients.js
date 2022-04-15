138d69cf1342ebf8758267e872662a22751d98b7
Jeyna Kim <jeyna.kimm@gmail.com>
2022-04-15 21:50:45 +0000

    ingredients

---
+++ lighthouse-js-fundamentals/ingredients.js
@@ -0,0 +1,22 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+
+while (ingredients < ingredients.length) {
+  console.log(ingredients);
+  ingredients++
+}
+
+// Write a for loop that prints out the contents of ingredients:
+for (let i = 0; i < ingredients.length; i++) {
+  ingredients[i];
+}
+console.log(ingredients);
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+for (let i = 0; i < ingredients.length; i++) {
+  ingredients[i];
+}
+console.log(ingredients.reverse);
+
